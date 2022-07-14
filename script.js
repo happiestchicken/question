a301bd285b60c9f68fafb22762cdecf4ef8007ca
iamkevin <kevinmcmackin1@gmail.com>
2022-07-14 18:54:42 -0300

    one

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-alert('hello');
+function toggleText() {
+    var text = document.getElementById("demo");
+    if (text.style.display === "none") {
+      text.style.display = "block";
+    } else {
+      text.style.display = "none";
+    }
+  }
 
-function myfunction() {
-    alert("I am cool");
+const questions = Array("orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,", "What?", "when?")
+const answers = Array("song1", "song2", "song3");
+
+function randomQuestion() {
+  const len = answers.length;
+  const rnd = Math.floor(Math.random() * len);
+  document.getElementById('randomQuestion').value = questions[rnd];
+  document.getElementById('randomAnswer').value = answers[rnd];
 }
